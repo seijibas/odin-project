f26d3be8f31f3ca7ce846c67c99a86cf23cabe22
Seiji Roberto Basaglia <seiji.basaglia@gmail.com>
2016-05-23 12:25:01 -0300

    Create problem2.js

---
+++ project-euler/problem2.js
@@ -0,0 +1,15 @@
+var numero=1;
+var ultimo=0;
+var penultimo=0;
+var soma=0;
+
+while (numero<=4000000) {
+  if (numero%2 === 0) {
+    soma += numero;
+  }
+  penultimo = ultimo;
+  ultimo = numero;
+  numero = ultimo + penultimo;
+}
+
+console.log(soma);
